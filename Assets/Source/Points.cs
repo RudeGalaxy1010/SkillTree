e6cd4e48441fc55f0885e3a6490f7edee41c7990
RudeGalaxy1010 <Karev007@bk.ru>
2023-04-25 22:47:57 +0300

    Points logic

---
+++ Assets/Source/Points.cs
@@ -0,0 +1,49 @@
+using System;
+
+public class Points
+{
+    private const string NegativeArgumentExceptionMessage = "Value can't be less than zero";
+
+    public event Action<int> Changed;
+
+    private int _value;
+
+    public Points(int startValue = 0)
+    {
+        _value = startValue;
+    }
+
+    public int Value => _value;
+
+    public void Add(int value)
+    {
+        if (value == 0)
+        {
+            return;
+        }
+
+        if (value <= 0)
+        {
+            throw new ArgumentException(NegativeArgumentExceptionMessage);
+        }
+
+        _value += value;
+        Changed?.Invoke(value);
+    }
+
+    public void Remove(int value)
+    {
+        if (value == 0)
+        {
+            return;
+        }
+
+        if (value <= 0)
+        {
+            throw new ArgumentException(NegativeArgumentExceptionMessage);
+        }
+
+        _value -= value;
+        Changed?.Invoke(value);
+    }
+}
---
+++ Assets/Source/Starter.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Starter : MonoBehaviour
+{
+    private Points _points;
+
+    private void Start()
+    {
+        _points = new Points();
+    }
+}
